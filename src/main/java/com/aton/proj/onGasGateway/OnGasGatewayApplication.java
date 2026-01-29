a990c203db3fd901f5130e5d2ef632007638fd91
Dario.Cescon <Dario.Cescon@aton.com>
2026-01-29 13:02:55 +0100

    Init

---
+++ src/main/java/com/aton/proj/onGasGateway/OnGasGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.aton.proj.onGasGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnGasGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnGasGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aton/proj/onGasGateway/OnGasGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aton.proj.onGasGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnGasGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
